84372403004e577670cdc372bb62463143c0e2ff
Lars Bahner <lars.bahner@gmail.com>
2018-01-25 17:51:50 +0100

    Some niceties in dependecies

--- manifests/init.pp
+++ manifests/init.pp
@@ -92,7 +92,7 @@ class rt (
       ensure  => file,
       content => template('rt/rt.conf.erb'),
       group   => 'root',
-      notify => Exec['update-siteconfig'],
+      notify  => Exec['update-siteconfig'],
       ;
     '/etc/request-tracker4/RT_SiteConfig.d/51-dbconfig-common':
       ensure  => file,
@@ -103,7 +103,10 @@ class rt (
 
   exec {
     'update-siteconfig':
-      command => '/usr/sbin/update-rt-siteconfig-4';
+      command     => '/usr/sbin/update-rt-siteconfig-4',
+      subscribe   => File['/etc/request-tracker4/rt.conf'],
+      refreshonly => true,
+      ;
   }
 
   create_resources('rt::queue', $queues)
